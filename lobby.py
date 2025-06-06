e42217a70f4cf2d4fd5b3fee9219090eeef49e41
Simeon Maxein <smaxein@gmail.com>
2025-06-06 15:41:48 +0200

    Add __main__ guard to lobby.py to prevent side-effects on import

--- lobby.py
+++ lobby.py
@@ -10,14 +10,15 @@ from protocols.gg2 import GG2LobbyRegV1, GG2LobbyQueryV1Factory
 from protocols.newstyle import NewStyleReg, NewStyleListFactory
 import weblist
 
-serverList = GameServerList()
-reactor.listenUDP(config.GG2_PORT, GG2LobbyRegV1(serverList))
-reactor.listenUDP(config.NEWSTYLE_PORT, NewStyleReg(serverList))
-reactor.listenTCP(config.GG2_PORT, GG2LobbyQueryV1Factory(serverList))
-reactor.listenTCP(config.NEWSTYLE_PORT, NewStyleListFactory(serverList))
+if __name__ == "__main__":
+    serverList = GameServerList()
+    reactor.listenUDP(config.GG2_PORT, GG2LobbyRegV1(serverList))
+    reactor.listenUDP(config.NEWSTYLE_PORT, NewStyleReg(serverList))
+    reactor.listenTCP(config.GG2_PORT, GG2LobbyQueryV1Factory(serverList))
+    reactor.listenTCP(config.NEWSTYLE_PORT, NewStyleListFactory(serverList))
 
-webres = twisted.web.static.File("httpdocs")
-webres.putChild(b"status", weblist.LobbyStatusResource(serverList))
+    webres = twisted.web.static.File("httpdocs")
+    webres.putChild(b"status", weblist.LobbyStatusResource(serverList))
 
-reactor.listenTCP(config.WEB_PORT, twisted.web.server.Site(webres))
-reactor.run()
+    reactor.listenTCP(config.WEB_PORT, twisted.web.server.Site(webres))
+    reactor.run()
